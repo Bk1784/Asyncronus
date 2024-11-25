35d73427255d007d2bcb1387813ee13db4ff29c8
bk1784 <futonabuu456@gmail.com>
2024-11-26 02:47:45 +0700

    menambahkan methode getData() dan memodifikasi pubsec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -28,6 +31,13 @@ class FuturePage extends StatefulWidget {
 }
 
 class _FuturePageState extends State<FuturePage> {
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/junbDwAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
+  }
+
   String result = '';
   @override
   Widget build(BuildContext context) {
@@ -36,17 +46,17 @@ class _FuturePageState extends State<FuturePage> {
         title: const Text('Back From Future'),
       ),
       body: Center(
-        child: Column(children: [
-          const Spacer(),
-          ElevatedButton(
-            onPressed: (){}, 
-            child: const Text('GO!')),
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(onPressed: () {}, child: const Text('GO!')),
             const Spacer(),
             Text(result),
             const Spacer(),
             const CircularProgressIndicator(),
             const Spacer(),
-        ],),
+          ],
+        ),
       ),
     );
   }
