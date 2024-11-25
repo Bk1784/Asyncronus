bcbf3b35b9ea7988f682d577870c54e8bd3ba66e
bk1784 <futonabuu456@gmail.com>
2024-11-26 03:54:44 +0700

    menambahkan return One Two Three dan pemanggilan fungsi count pada elevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,31 @@ class FuturePage extends StatefulWidget {
 }
 
 class _FuturePageState extends State<FuturePage> {
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
+
   Future<Response> getData() async {
     const authority = 'www.googleapis.com';
     const path = '/books/v1/volumes/junbDwAAQBAJ';
@@ -51,17 +76,22 @@ class _FuturePageState extends State<FuturePage> {
           children: [
             const Spacer(),
             ElevatedButton(
-                onPressed: () {
-                  setState(() {});
-                  getData().then((value) {
-                    result = value.body.toString().substring(0, 450);
-                    setState(() {});
-                  }).catchError((_) {
-                    result = 'An error occured';
-                    setState(() {});
-                  });
-                },
-                child: const Text('GO!')),
+              child: Text('GO!'),
+              onPressed: () {
+                count();
+              },
+              // onPressed: () {
+              //   setState(() {});
+              //   getData().then((value) {
+              //     result = value.body.toString().substring(0, 450);
+              //     setState(() {});
+              //   }).catchError((_) {
+              //     result = 'An error occured';
+              //     setState(() {});
+              //   });
+              // },
+              // child: const Text('GO!')
+            ),
             const Spacer(),
             Text(result),
             const Spacer(),
