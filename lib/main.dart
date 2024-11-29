9f4ea9e4b477eea88aa50f1334c25dddddec50c6
bk1784 <futonabuu456@gmail.com>
2024-11-29 17:06:45 +0700

    membuat navigation route dengan future function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:books/geolocation.dart';
+import 'package:books/navigation_first.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LocationScreen(),
+      home: NavigationFirst(),
     );
   }
 }
